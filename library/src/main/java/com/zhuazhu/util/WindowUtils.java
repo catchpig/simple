b3e6c696858aad36780f7aefec7da2173844c431
tao <327785185@qq.com>
2017-04-03 00:46:21 +0800

    增加读取meta-data的函数

--- library/src/main/java/com/zhuazhu/util/WindowUtils.java
+++ library/src/main/java/com/zhuazhu/util/WindowUtils.java
@@ -2,6 +2,7 @@ package com.zhuazhu.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.telephony.TelephonyManager;
@@ -17,21 +18,20 @@ import android.view.inputmethod.InputMethodManager;
 
 public class WindowUtils {
     /**
-     * 获取版本code
-     *
+     * 获取meta-data的value
      * @param context
+     * @param key
      * @return
-     * @throws Exception
+     * @throws PackageManager.NameNotFoundException
      */
-    public static int getVersionCode(Context context) throws Exception {
-        // 获取packagemanager的实例
-        PackageManager packageManager = context.getPackageManager();
-        // getPackageName()是你当前类的包名，0代表是获取版本信息
-        PackageInfo packInfo = packageManager.getPackageInfo(
-                context.getPackageName(), 0);
-        int version = packInfo.versionCode;
-        return version;
+    public static String metaData(Context context, String key) throws
+            PackageManager.NameNotFoundException {
+        ApplicationInfo applicationInfo = context.getPackageManager()
+                .getApplicationInfo(context.getPackageName(), PackageManager
+                        .GET_META_DATA);
+        return applicationInfo.metaData.getString("key");
     }
+
     /**
      * 获取版本code
      *
@@ -39,31 +39,51 @@ public class WindowUtils {
      * @return
      * @throws Exception
      */
-    public static String getVersionName(Context context) {
+    public static int versionCode(Context context) throws Exception {
+        // 获取packagemanager的实例
+        PackageManager packageManager = context.getPackageManager();
+        // getPackageName()是你当前类的包名，0代表是获取版本信息
+        PackageInfo packInfo = packageManager.getPackageInfo(context
+                .getPackageName(), 0);
+        int version = packInfo.versionCode;
+        return version;
+    }
+
+    /**
+     * 获取版本
+     *
+     * @param context
+     * @return
+     * @throws Exception
+     */
+    public static String versionName(Context context) {
         // 获取packagemanager的实例
         PackageManager packageManager = context.getPackageManager();
         // getPackageName()是你当前类的包名，0代表是获取版本信息
         PackageInfo packInfo = null;
         try {
-            packInfo = packageManager.getPackageInfo(
-                    context.getPackageName(), 0);
+            packInfo = packageManager.getPackageInfo(context.getPackageName()
+                    , 0);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if(packInfo==null){
+        if (packInfo == null) {
             return null;
         }
         return packInfo.versionName;
     }
+
     /**
      * 获取设备id
      *
      * @return
      */
-    public static String getDeviceId(Context context) {
-        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+    public static String deviceId(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService
+                (Context.TELEPHONY_SERVICE);
         return tm.getDeviceId();
     }
+
     /**
      * 隐藏软键盘
      *
@@ -73,6 +93,6 @@ public class WindowUtils {
         InputMethodManager manager = (InputMethodManager) activity
                 .getSystemService(Context.INPUT_METHOD_SERVICE);
         manager.hideSoftInputFromWindow(activity.getCurrentFocus()
-                .getWindowToken(),0);
+                .getWindowToken(), 0);
     }
 }
