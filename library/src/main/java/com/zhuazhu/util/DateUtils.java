ccaf0f7131cb917dcd36ed40e9565e158c21952f
tao <327785185@qq.com>
2017-04-06 10:47:38 +0800

    增加两个时间毫秒数计算时间天数

--- library/src/main/java/com/zhuazhu/util/DateUtils.java
+++ library/src/main/java/com/zhuazhu/util/DateUtils.java
@@ -13,6 +13,29 @@ import java.util.Date;
 public class DateUtils {
     /**
      * 获取两个日期之间相差的天数
+     * @param startTimeMillis
+     * @param endTimeMillis
+     * @return
+     */
+    public static int gapDays(long startTimeMillis,long endTimeMillis){
+        Calendar fromCalendar = Calendar.getInstance();
+        fromCalendar.setTimeInMillis(startTimeMillis);
+        fromCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        fromCalendar.set(Calendar.MINUTE, 0);
+        fromCalendar.set(Calendar.SECOND, 0);
+        fromCalendar.set(Calendar.MILLISECOND, 0);
+
+        Calendar toCalendar = Calendar.getInstance();
+        toCalendar.setTimeInMillis(endTimeMillis);
+        toCalendar.set(Calendar.HOUR_OF_DAY, 0);
+        toCalendar.set(Calendar.MINUTE, 0);
+        toCalendar.set(Calendar.SECOND, 0);
+        toCalendar.set(Calendar.MILLISECOND, 0);
+
+        return (int) ((toCalendar.getTime().getTime() - fromCalendar.getTime().getTime()) / (1000 * 60 * 60 * 24));
+    }
+    /**
+     * 获取两个日期之间相差的天数
      * @param startCalendar
      * @param endDate
      * @return
