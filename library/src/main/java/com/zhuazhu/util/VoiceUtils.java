a1fa483004f614af7709e418172476d6a07e57aa
tao <327785185@qq.com>
2017-12-18 09:16:30 +0800

    播放语音增加按顺序播放

--- library/src/main/java/com/zhuazhu/util/VoiceUtils.java
+++ library/src/main/java/com/zhuazhu/util/VoiceUtils.java
@@ -2,6 +2,7 @@ package com.zhuazhu.util;
 
 import android.content.Context;
 import android.media.MediaPlayer;
+import android.support.annotation.RawRes;
 
 /**
  * Created by tao on 2016/11/24.
@@ -14,7 +15,16 @@ public class VoiceUtils {
      * @param context
      * @param raw
      */
-    public static void play(Context context,int raw){
+    public static void play(Context context,@RawRes int raw){
+        MediaPlayer player = MediaPlayer.create(context,raw);
+        player.start();
+    }
+    /**
+     * 播放语音(按顺序播放)
+     * @param context
+     * @param raw
+     */
+    public synchronized static void playSynchronized(Context context,@RawRes int raw){
         MediaPlayer player = MediaPlayer.create(context,raw);
         player.start();
     }
